4b1876170ebd1784d4a337671058f41ed62e983a
Dabin <dorothy9795@gmail.com>
2024-05-25 19:37:42 +0900

    반복횟수 다시 조정

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -9,9 +9,9 @@ import matplotlib.pylab as plt
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 # 하이퍼파라미터
-iters_num = 60  # 반복 횟수를 적절히 설정한다.
+iters_num = 1000  # 반복 횟수를 적절히 설정한다.
 train_size = x_train.shape[0]
-batch_size = 10   # 미니배치 크기
+batch_size = 30   # 미니배치 크기
 learning_rate = 0.1
 
 train_loss_list = []
