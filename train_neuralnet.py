60c01ea072b5287f62f62d5ebd1fe41d7e590fe3
Dabin <dorothy9795@gmail.com>
2024-05-25 19:12:06 +0900

    반복 횟수, 배치사이즈 변경

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -3,14 +3,15 @@ from mnist_dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 import matplotlib.pylab as plt
 
+# 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-# 하이퍼 파라미터
-iters_num = 30 # 반복 횟수
+# 하이퍼파라미터
+iters_num = 10000  # 반복 횟수를 적절히 설정한다.
 train_size = x_train.shape[0]
-batch_size = 100 # 미니 배치 사이즈
+batch_size = 100   # 미니배치 크기
 learning_rate = 0.1
 
 train_loss_list = []
@@ -21,25 +22,23 @@ test_acc_list = []
 iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
-
-    print(i)
-    # 미니 배치 획득
+    # 미니배치 획득
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-
+    
     # 기울기 계산
     grad = network.numerical_gradient(x_batch, t_batch)
-    # grad = network.gradient(x_batch, t_batch) # 성능 개선판!
-
+    #grad = network.gradient(x_batch, t_batch)
+    
     # 매개변수 갱신
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
-
+    
     # 학습 경과 기록
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-
+    
     # 1에폭당 정확도 계산
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
@@ -47,7 +46,7 @@ for i in range(iters_num):
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
-        
+
 # 그래프 그리기
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(len(train_acc_list))
