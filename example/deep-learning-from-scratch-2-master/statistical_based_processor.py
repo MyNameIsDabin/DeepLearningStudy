2d779c59ad5ec222e49418761d6227805708c38b
Dabin <dorothy9795@gmail.com>
2024-11-17 00:53:00 +0900

    통계 기반 방식 단어 유사도 순위 매기기

---
+++ example/deep-learning-from-scratch-2-master/statistical_based_processor.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.util import preprocess
+
+text = 'You say goodbye and I say hello.'
+
+corpus, word_to_id, id_to_word = preprocess(text)
+
+print(corpus)
+print(id_to_word)
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+        
+    return co_matrix
+
